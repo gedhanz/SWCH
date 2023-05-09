e6ab5442fdd4155b90203ce8870035d61026d364
gedhanz <gedhanz69@gmail.com>
2023-05-09 15:54:20 +0900

    PROJECT INIT

--- app/src/main/java/com/example/aswitch/MainActivity.kt
+++ app/src/main/java/com/example/aswitch/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.aswitch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.naver.maps.map.MapView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapView = findViewById<MapView>(R.id.map_view)
+        mapView.onCreate(savedInstanceState)
     }
 }
