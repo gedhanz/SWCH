6398431e8d55da880f0a0d06ce4172e08b9e29b6
gedhanz <gedhanz69@gmail.com>
2023-05-10 16:25:35 +0900

    BottomViewNavigation + Naver Map

--- app/src/main/java/com/example/aswitch/MainActivity.kt
+++ app/src/main/java/com/example/aswitch/MainActivity.kt
@@ -6,18 +6,18 @@ import com.google.android.material.navigation.NavigationBarView
 
 
 class MainActivity : AppCompatActivity() {
-    var homeFragment: HomeFragment? = null
-    var infoFragment: InfoFragment? = null
-    var settingFragment: SettingFragment? = null
+    var homeFragment: HomeFragment? = null //첫번쨰 화면 'HomeFragment' 이름 추후변경
+    var infoFragment: InfoFragment? = null //두번쨰 화면
+    var settingFragment: SettingFragment? = null //세번째 화면
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         homeFragment = HomeFragment()
         infoFragment = InfoFragment()
         settingFragment = SettingFragment()
-        supportFragmentManager.beginTransaction().replace(R.id.containers, homeFragment!!).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.containers, homeFragment!!).commit() //초기화면 설정
         val navigationBarView = findViewById<NavigationBarView>(R.id.bottom_navigationview)
-        navigationBarView.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener { item ->
+        navigationBarView.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener { item -> //화면 교체 기능
             when (item.itemId) {
                 R.id.home -> {
                     supportFragmentManager.beginTransaction().replace(R.id.containers,
