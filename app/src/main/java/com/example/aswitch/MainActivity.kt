75c4b4925f335d0857ce0e25a61933541c487a5c
gedhanz <gedhanz69@gmail.com>
2023-05-10 15:42:53 +0900

    BottomViewNavigation

--- app/src/main/java/com/example/aswitch/MainActivity.kt
+++ app/src/main/java/com/example/aswitch/MainActivity.kt
@@ -1,21 +1,45 @@
 package com.example.aswitch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import com.naver.maps.map.MapFragment
-
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationBarView
 
 
 class MainActivity : AppCompatActivity() {
+    var homeFragment: HomeFragment? = null
+    var infoFragment: InfoFragment? = null
+    var settingFragment: SettingFragment? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val fm = supportFragmentManager
-        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
-            ?: MapFragment.newInstance().also {
-                fm.beginTransaction().add(R.id.map, it).commit()
+        homeFragment = HomeFragment()
+        infoFragment = InfoFragment()
+        settingFragment = SettingFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.containers, homeFragment!!).commit()
+        val navigationBarView = findViewById<NavigationBarView>(R.id.bottom_navigationview)
+        navigationBarView.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.containers,
+                        homeFragment!!
+                    )
+                        .commit()
+                    return@OnItemSelectedListener true
+                }
+                R.id.setting -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.containers, settingFragment!!).commit()
+                    return@OnItemSelectedListener true
+                }
+                R.id.info -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.containers,
+                        infoFragment!!
+                    )
+                        .commit()
+                    return@OnItemSelectedListener true
+                }
             }
+            false
+        })
     }
 }
